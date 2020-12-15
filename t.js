fc14545b537744a320a5299b61eb915ace8fdfd8
teriyaki <kaisatec@users.noreply.github.com>
2020-12-14 22:47:43 -0800

    Add files via upload

--- t.js
+++ t.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var SQLOptions={
-    host: localhost,//",//192.168.0.101
+    host: "localhost",//",//192.168.0.101
     port:  "3306",
     database:"customer",
     user:"clientlocal",// "root",
